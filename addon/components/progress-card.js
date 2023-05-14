c6f4bf5fc079956470ebd2c86f296d50297dd712
Anand Gopinath <reachoutanandgopinath@gmail.com>
2023-05-14 17:32:02 +0530

    changes on eslint

--- addon/components/progress-card.js
+++ addon/components/progress-card.js
@@ -1,8 +1,8 @@
 import Component from '@glimmer/component';
 import { htmlSafe } from '@ember/template';
-
 export default class ProgressCardComponent extends Component {
-    get style() {
-        return htmlSafe("background:"+ this.args.color);
-    }
+  get style() {
+    return htmlSafe(`background:` + this.args.color);
+  }
 }
+
